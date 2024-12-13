df0c5fcd3523bfa6bfbf18326c00941c74d38dd7
Arombolosch75 <99829083+Arombolosch75@users.noreply.github.com>
2024-12-13 20:11:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,144 @@
+function door1(div1){
+    div1.innerHTML = 'Nimm Dir etwas Ruhe und Du wirst mehr Energie haben.';
+    div1.style.fontSize = '15px';
+    div1.style.backgroundColor = 'rgb(9, 96, 27)';
+}
+
+function door2(div2){
+    div2.innerHTML = 'Nur wer seinen Träumen Hügel wachsen lässt wir fliegen.';
+    div2.style.fontSize = '15px';
+    div2.style.backgroundColor = 'rgb(235, 64, 52)';
+}
+
+function door3(div3){
+    div3.innerHTML = 'Die Welt ist schön, weil Du mit drauf bist.';
+    div3.style.fontSize = '15px';
+    div3.style.backgroundColor = 'rgb(212, 199, 19)';
+}
+
+function door4(div4){
+    div4.innerHTML = 'Tu was Du liebst, Lebe den Moment, Glaube an Dich selbst.';
+    div4.style.fontSize = '15px';
+    div4.style.backgroundColor = 'rgb(9, 112, 78)';
+}
+
+function door5(div5){
+    div5.innerHTML = 'Was auch immer gut für Deine Seele ist, mach es.';
+    div5.style.fontSize = '15px';
+    div5.style.backgroundColor = 'rgb(148, 10, 26)';
+}
+
+function door6(div6){
+    div6.innerHTML = 'Liebe das Leben! Es ist das einzige Geschenk, das Du nicht zweimal erhälst.';
+    div6.style.fontSize = '15px';
+    div6.style.backgroundColor = 'rgb(46, 66, 13)';
+}
+
+function door7(div7){
+    div7.innerHTML = 'Ein Leben ohne Träume ist wie ein Garten ohne Blumen.';
+    div7.style.fontSize = '15px';
+    div7.style.backgroundColor = 'rgb(29, 153, 112)';
+}
+
+function door8(div8){
+    div8.innerHTML = 'Bleib in der nähe von Menschen, die sich wie Sonnenschein anfühlen.';
+    div8.style.fontSize = '15px';
+    div8.style.backgroundColor = 'rgb(113, 168, 54)';
+}
+
+function door9(div9){
+    div9.innerHTML = 'Ehrlichkeit ist etwas für starke Menschen, Schwache wählen die Lüge.';
+    div9.style.fontSize = '15px';
+    div9.style.backgroundColor = 'rgb(209, 93, 21)';
+}
+
+function door10(div10){
+    div10.innerHTML = 'Manchmal ist ein Rückzug nötig, um Herz und Seele zu schützen.';
+    div10.style.fontSize = '15px';
+    div10.style.backgroundColor = 'rgb(20, 39, 204)';
+}
+
+function door11(div11){
+    div11.innerHTML = 'Wenn Du nichts änderst, ändert sich nichts.';
+    div11.style.fontSize = '15px';
+    div11.style.backgroundColor = 'rgb(21, 189, 164)';
+}
+
+function door12(div12){
+    div12.innerHTML = 'Manchmal denkt man zu viel nach, anstatt einfach glücklich zu sein.';
+    div12.style.fontSize = '15px';
+    div12.style.backgroundColor = 'rgb(9, 112, 784)';
+}
+
+function door13(div13){
+    div13.innerHTML = 'Kraft tanken kann man nur da, wo man sich wohl fühlt.';
+    div13.style.fontSize = '15px';
+    div13.style.backgroundColor = 'rgb(54, 142, 209)';
+}
+
+function door14(div14){
+    div14.innerHTML = 'Starke Menschen haben nie eine einfache Vergangenheit.';
+    div14.style.fontSize = '15px';
+    div14.style.backgroundColor = 'rgb(83, 102, 21)';
+}
+
+function door15(div15){
+    div15.innerHTML = 'Manche Menschen spüren den Regen, andere werden nur nass.';
+    div15.style.fontSize = '15px';
+    div15.style.backgroundColor = 'rgb(102, 16, 67)';
+}
+
+function door16(div16){
+    div16.innerHTML = 'Kopf hoch, sonst kannst Du die Sterne nicht mehr sehen.';
+    div16.style.fontSize = '15px';
+    div16.style.backgroundColor = 'rgb(1, 56, 51)';
+}
+
+function door17(div17){
+    div17.innerHTML = 'Lass immer ein wenig Platz in Deinem Herzen für das Unvorstellbare.';
+    div17.style.fontSize = '15px';
+    div17.style.backgroundColor = 'rgb(1, 56, 51)';
+}
+
+function door18(div18){
+    div18.innerHTML = 'Wir leben alle unter dem gleichen Himmel, aber wir haben nicht den gleichen Horizont.';
+    div18.style.fontSize = '12px';
+    div18.style.backgroundColor = 'rgb(19, 105, 39)';
+}
+
+function door19(div19){
+    div19.innerHTML = 'Lerne auf Deine innere Stimme zu hören, anstatt auf den Lärm von außen zu achten.';
+    div19.style.fontSize = '15px';
+    div19.style.backgroundColor = 'rgb(56, 77, 140)';
+}
+
+function door20(div20){
+    div20.innerHTML = 'Liebe ist: "Wenn Du morgens von einer Fellnase geweckt wirst und Abends mit ihr einschläfst."';
+    div20.style.fontSize = '12px';
+    div20.style.backgroundColor = 'rgb(145, 9, 43)';
+}
+
+function door21(div21){
+    div21.innerHTML = 'Gestern ist Geschichte, Morgen ist ein Geheimnis, Heute ist das Leben.';
+    div21.style.fontSize = '15px';
+    div21.style.backgroundColor = 'rgb(176, 137, 53)';
+}
+
+function door22(div22){
+    div22.innerHTML = 'Jeder hat ein Datum, das sein Leben verändert hat';
+    div22.style.fontSize = '15px';
+    div22.style.backgroundColor = 'rgb(9, 112, 78)';
+}
+
+function door23(div23){
+    div23.innerHTML = 'Manchmal sollte man sich lieber Nudeln als Sorgen machen';
+    div23.style.fontSize = '15px';
+    div23.style.backgroundColor = 'rgb(9, 112, 78)';
+}
+
+function door24(div24){
+    div24.innerHTML = 'Glücklich ist, wer daran glaubtm, dass es im Leben nie zu spät ist, um neu anzufangen.';
+    div24.style.fontSize = '12px';
+    div24.style.backgroundColor = 'rgb(9, 112, 78)';
+    
+}
